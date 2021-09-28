b9e9983bf2c04268b7a2def0ffadd78021c0c3d2
Tatyana Ovchinnikova <84726881+TOvchinnikova@users.noreply.github.com>
2021-09-28 22:20:29 +0500

    Implemented class Settings

--- app/src/main/java/com/t_ovchinnikova/android/scandroid_2/Settings.kt
+++ app/src/main/java/com/t_ovchinnikova/android/scandroid_2/Settings.kt
@@ -2,7 +2,37 @@ package com.t_ovchinnikova.android.scandroid_2
 
 import android.content.Context
 
+private const val SHARED_PREFERENCES_NAME = "SHARED_PREFERENCES_NAME"
+private const val VIBRATE = "vibrate"
+private const val SAVE_SCANNED_BARCODES_TO_HISTORY = "save_scanned_barcodes_to_history"
+private const val FLASH = "flash"
+
 class Settings(private val context: Context) {
 
+    private val sharedPreferences by lazy {
+        context.getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
+    }
+
+    var vibrate: Boolean
+        get() = get(VIBRATE, true)
+        set(value) = set(VIBRATE, value)
+
+    var saveScannedBarcodesToHistory: Boolean
+        get() = get(SAVE_SCANNED_BARCODES_TO_HISTORY, true)
+        set(value) = set(SAVE_SCANNED_BARCODES_TO_HISTORY, value)
+
+    var flash: Boolean
+    get() = get(FLASH)
+    set(value) = set(FLASH, value)
+
+    private fun get(key: String, default: Boolean = false): Boolean {
+        return sharedPreferences.getBoolean(key, default)
+    }
+
+    private fun set(key: String, value: Boolean) {
+        sharedPreferences.edit()
+            .putBoolean(key, value)
+            .apply()
+    }
 
 }
