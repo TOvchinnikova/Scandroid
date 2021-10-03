aec6211a78104c8c7aa8a60de61792e5fcaa11ca
Tatyana Ovchinnikova <84726881+TOvchinnikova@users.noreply.github.com>
2021-10-03 20:25:03 +0500

    Implemented actions with the search view

--- app/src/main/java/com/t_ovchinnikova/android/scandroid_2/presentation/HistoryFragment.kt
+++ app/src/main/java/com/t_ovchinnikova/android/scandroid_2/presentation/HistoryFragment.kt
@@ -9,6 +9,12 @@ import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.t_ovchinnikova.android.scandroid_2.databinding.FragmentScanningHistoryBinding
+import android.app.Activity
+import android.util.Log
+import android.view.KeyEvent
+import android.view.inputmethod.InputMethodManager
+import androidx.core.widget.doOnTextChanged
+
 
 class HistoryFragment : Fragment() {
 
@@ -29,12 +35,54 @@ class HistoryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        setupView()
         setupRecyclerView()
         setupSwipeListener(rvHistoryList)
-        viewModel.codeListLiveData.observe(viewLifecycleOwner, {
-            codeListAdapter.submitList(it)
-        })
+        setupViewModel()
+    }
+
+    private fun setupView() {
+        with(binding) {
+            with(searchContainer) {
+                ivClear.setOnClickListener {
+                    etSearch.setText("")
+                    hideKeyboard(requireActivity())
+                    view?.clearFocus()
+                }
+                etSearch.setOnFocusChangeListener { _, isFocused ->
+                    if (isFocused) {
+                        ivClear.visibility = View.VISIBLE
+                    } else {
+                        ivClear.visibility = View.GONE
+                    }
+                }
+                etSearch.doOnTextChanged { text, _, _, _ ->
+                    Log.d("MyLog", "$text")
+                    filterList(text.toString())
+                }
+                etSearch.setOnKeyListener { view, keyCode, _ ->
+                    if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                        hideKeyboard(requireActivity())
+                        view?.clearFocus()
+                    }
+                    return@setOnKeyListener true
+                }
+            }
+        }
+    }
+
+    private fun filterList(text: String) {
+
+    }
+
+    private fun hideKeyboard(activity: Activity) {
+        val imm: InputMethodManager =
+            activity.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        var view = activity.currentFocus
+        if (view == null) {
+            view = View(activity)
+        }
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
     private fun setupRecyclerView() {
@@ -75,6 +123,12 @@ class HistoryFragment : Fragment() {
             }
         }
         ItemTouchHelper(callback).attachToRecyclerView(rvHistory)
+    }
+
+    private fun setupViewModel() {
+        viewModel.codeListLiveData.observe(viewLifecycleOwner, {
+            codeListAdapter.submitList(it)
+        })
     }
 
     companion object {
