f6880b2945571d004a6bf0ed4f0a9cf9a09088e9
Tatyana Ovchinnikova <84726881+TOvchinnikova@users.noreply.github.com>
2021-10-13 23:27:15 +0500

    Changed the color of the icons in the dialog.

--- app/src/main/java/com/t_ovchinnikova/android/scandroid_2/presentation/DeleteCodeDialogFragment.kt
+++ app/src/main/java/com/t_ovchinnikova/android/scandroid_2/presentation/DeleteCodeDialogFragment.kt
@@ -3,7 +3,6 @@ package com.t_ovchinnikova.android.scandroid_2.presentation
 import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
-import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.t_ovchinnikova.android.scandroid_2.R
@@ -22,7 +21,7 @@ class DeleteCodeDialogFragment : DialogFragment() {
         }
         return MaterialAlertDialogBuilder(requireContext(), R.style.AlertDialogTheme)
             .setCancelable(false)
-            .setIcon(R.drawable.ic_delete_light_blue)
+            .setIcon(R.drawable.ic_delete_dialog)
             .setTitle(R.string.delete_question_dialog)
             .setPositiveButton(R.string.yes, listener)
             .setNegativeButton(R.string.no, listener)
--- app/src/main/java/com/t_ovchinnikova/android/scandroid_2/presentation/EditCodeNoteDialogFragment.kt
+++ app/src/main/java/com/t_ovchinnikova/android/scandroid_2/presentation/EditCodeNoteDialogFragment.kt
@@ -3,7 +3,6 @@ package com.t_ovchinnikova.android.scandroid_2.presentation
 import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
-import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.t_ovchinnikova.android.scandroid_2.R
@@ -28,7 +27,7 @@ class EditCodeNoteDialogFragment : DialogFragment() {
 
         return MaterialAlertDialogBuilder(requireContext(), R.style.AlertDialogTheme)
             .setCancelable(false)
-            .setIcon(R.drawable.ic_edit_light_blue)
+            .setIcon(R.drawable.ic_edit_light_dialog)
             .setTitle(R.string.note)
             .setView(noteBinding.root)
             .setPositiveButton(R.string.save, listener)
