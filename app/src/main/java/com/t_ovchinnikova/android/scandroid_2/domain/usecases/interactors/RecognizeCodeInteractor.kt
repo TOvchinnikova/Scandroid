7065a4482bb7dec45509d30bdcc5a69b7d382001
Tatyana Ovchinnikova <84726881+TOvchinnikova@users.noreply.github.com>
2022-11-24 23:02:52 +0500

    Refactoring

--- app/src/main/java/com/t_ovchinnikova/android/scandroid_2/domain/usecases/interactors/RecognizeCodeInteractor.kt
+++ app/src/main/java/com/t_ovchinnikova/android/scandroid_2/domain/usecases/interactors/RecognizeCodeInteractor.kt
@@ -1,8 +1,6 @@
 package com.t_ovchinnikova.android.scandroid_2.domain.usecases.interactors
 
-import com.google.android.gms.tasks.Task
 import com.google.mlkit.vision.barcode.BarcodeScanner
-import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
 import com.t_ovchinnikova.android.scandroid_2.domain.Code
@@ -10,8 +8,7 @@ import com.t_ovchinnikova.android.scandroid_2.domain.usecases.RecognizeCodeUseCa
 import com.t_ovchinnikova.android.scandroid_2.presentation.ScanResultListener
 
 class RecognizeCodeInteractor(
-    private val scanner: BarcodeScanner//,
-    //private val listener: ScanResultListener
+    private val scanner: BarcodeScanner
 ) : RecognizeCodeUseCase {
 
     override fun invoke(image: InputImage, listener: ScanResultListener) {
@@ -29,9 +26,7 @@ class RecognizeCodeInteractor(
             rawValue?.let {
                 val format = barcode.format
                 val type = barcode.valueType
-                //val code =
-                    Code(text = rawValue, format = format, type = type)
-                //listener.onScanned(code)
+                Code(text = rawValue, format = format, type = type)
             }
         }
     }
