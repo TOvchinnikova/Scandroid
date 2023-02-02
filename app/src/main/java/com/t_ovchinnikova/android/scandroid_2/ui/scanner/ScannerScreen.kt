9d3143a0c30d3a772453dd07aa904dcf2d0826f3
t.ovchinnikova <t.ovchinnikova@evotor.ru>
2023-02-02 15:15:08 +0500

    Revert "ScannerScreen"
    This reverts commit 99b109b2c46d360a78466ee493cecf8dc3ef01b0.

--- app/src/main/java/com/t_ovchinnikova/android/scandroid_2/ui/scanner/ScannerScreen.kt
+++ app/src/main/java/com/t_ovchinnikova/android/scandroid_2/ui/scanner/ScannerScreen.kt
@@ -1,10 +1,14 @@
 package com.t_ovchinnikova.android.scandroid_2.ui.scanner
 
-import android.annotation.SuppressLint
 import android.content.Context
-import android.util.Rational
+import android.util.Log
+import android.view.OrientationEventListener
+import android.view.Surface
 import android.view.ViewGroup
-import androidx.camera.core.*
+import androidx.camera.core.Camera
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.Image
@@ -14,35 +18,41 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
-import com.canhub.cropper.CropImage.CancelledResult.rotation
 import com.t_ovchinnikova.android.scandroid_2.R
 import com.t_ovchinnikova.android.scandroid_2.domain.Code
 import com.t_ovchinnikova.android.scandroid_2.presentation.ScanResultListener
 import com.t_ovchinnikova.android.scandroid_2.presentation.viewmodel.ScanningViewModel
+import com.t_ovchinnikova.android.scandroid_2.presentation.viewmodel.SettingsViewModel
 import com.t_ovchinnikova.android.scandroid_2.ui.theme.ColorScannerButtonPanel
 import com.t_ovchinnikova.android.scandroid_2.utils.executor
+import com.t_ovchinnikova.android.scandroid_2.utils.getCameraProvider
 import com.t_ovchinnikova.android.scandroid_2.utils.vibrate
 import com.t_ovchinnikova.android.scandroid_2.views.ViewFinderOverlay
+import kotlinx.coroutines.launch
+import org.koin.android.ext.android.inject
 import org.koin.androidx.compose.get
+import org.koin.androidx.compose.inject
 import org.koin.androidx.compose.koinViewModel
 import org.koin.core.parameter.parametersOf
+import org.koin.java.KoinJavaComponent.inject
 
-@SuppressLint("UnsafeOptInUsageError")
 @Composable
 fun CameraPreview(
     modifier: Modifier = Modifier,
     scaleType: PreviewView.ScaleType = PreviewView.ScaleType.FILL_CENTER,
+    cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA,
     context: Context
 ) {
     val viewModel = koinViewModel<ScanningViewModel>()
 
+    val coroutineScope = rememberCoroutineScope()
     val previewCameraView = remember { PreviewView(context).apply {
         this.scaleType = scaleType
         layoutParams = ViewGroup.LayoutParams(
@@ -53,11 +63,8 @@ fun CameraPreview(
     val cameraProviderFuture =
         remember(context) { ProcessCameraProvider.getInstance(context) }
     val cameraProvider = remember(cameraProviderFuture) { cameraProviderFuture.get() }
-
     val lifecycleOwner = LocalLifecycleOwner.current
-
-    val cameraSelector = remember { buildCameraSelector() }
-    val cameraExecutor = context.executor
+    val camera: Camera? = null
     val analyzer = get<ImageAnalysis.Analyzer> {
         parametersOf(
             object : ScanResultListener {
@@ -73,33 +80,33 @@ fun CameraPreview(
             74
         )
     }
-
-        val configuration = LocalConfiguration.current
-        val screenHeight = configuration.screenHeightDp
-        val screenWidth = configuration.screenWidthDp
-
-    val imageAnalysis: ImageAnalysis = remember {
-        buildImageAnalysis(Rational(screenWidth, screenHeight).toInt()).apply {
-            setAnalyzer(cameraExecutor, analyzer)
-        }
-    }
+    var imageAnalysis: ImageAnalysis? = null
     AndroidView(
         factory = {
             cameraProviderFuture.addListener(
                 {
+                    val screenAspectRatio = getScreenAspectRatio()
+                    val rotation = display.rotation
+                    imageAnalysis = buildImageAnalysis(screenAspectRatio, rotation)
+                    //addOrientationEventListener(preview)
+                    imageAnalysis?.setAnalyzer(cameraExecutor, analyzer)
+
+
+                    val cameraSelector = buildCameraSelector()
+                    cameraProvider.unbindAll()
+
                     val previewUseCase = Preview.Builder()
                         .build()
                         .also {
                             it.setSurfaceProvider(previewCameraView.surfaceProvider)
                         }
 
-                    cameraProvider.unbindAll()
                     cameraProvider.bindToLifecycle(
                         lifecycleOwner,
                         cameraSelector,
-                        buildUseCaseGroup(previewUseCase, imageAnalysis)
+                        previewUseCase
                     )
-                }, cameraExecutor
+                }, context.executor
             )
 
             previewCameraView
@@ -150,18 +157,26 @@ private fun buildCameraSelector(): CameraSelector {
         .build()
 }
 
-private fun buildImageAnalysis(aspectRatio: Int): ImageAnalysis {
+private fun buildImageAnalysis(screenAspectRatio: Int, rotation: Int): ImageAnalysis {
     return ImageAnalysis.Builder()
-        .setTargetAspectRatio(aspectRatio)
+        .setTargetAspectRatio(screenAspectRatio)
         .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
         .setTargetRotation(rotation)
         .build()
 }
 
-@SuppressLint("UnsafeOptInUsageError")
-private fun buildUseCaseGroup(preview: Preview, imageAnalysis: ImageAnalysis): UseCaseGroup {
-    return UseCaseGroup.Builder()
-        .addUseCase(preview)
-        .addUseCase(imageAnalysis)
-        .build()
+private fun addOrientationEventListener(preview: Preview) {
+    val orientationEventListener = object : OrientationEventListener(()) {
+        override fun onOrientationChanged(orientation: Int) {
+            val rotation: Int = when (orientation) {
+                in 45..134 -> Surface.ROTATION_270
+                in 135..224 -> Surface.ROTATION_180
+                in 225..314 -> Surface.ROTATION_90
+                else -> Surface.ROTATION_0
+            }
+            preview.targetRotation = rotation
+            imageAnalysis?.targetRotation = rotation
+        }
+    }
+    orientationEventListener.enable()
 }
