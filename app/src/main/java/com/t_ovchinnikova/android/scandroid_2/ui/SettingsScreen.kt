a1374f00e2a3407dc7ca86217aa34fe017d00c9b
t.ovchinnikova <t.ovchinnikova@evotor.ru>
2023-02-02 15:15:13 +0500

    Revert "SettingsScreen Finished"
    This reverts commit c253536a3ceec61b32aad832489ba4fc7150770d.

--- app/src/main/java/com/t_ovchinnikova/android/scandroid_2/ui/SettingsScreen.kt
+++ app/src/main/java/com/t_ovchinnikova/android/scandroid_2/ui/SettingsScreen.kt
@@ -1,106 +1,30 @@
 package com.t_ovchinnikova.android.scandroid_2.ui
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Checkbox
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.t_ovchinnikova.android.scandroid_2.R
-import com.t_ovchinnikova.android.scandroid_2.ui.theme.ColorPrimary
-import com.t_ovchinnikova.android.scandroid_2.ui.theme.ScandroidTheme
 
 @Preview
 @Composable
 fun SettingsScreen() {
-    ScandroidTheme {
-        Scaffold(
-            topBar = {
-                TopAppBar(
-                    title = {
-                        Text(text = stringResource(id = R.string.settings))
-                    }
-                )
-            }
-        ) {
-            Column(
-                modifier = Modifier
-                    .padding(horizontal = 16.dp)
-                    .padding(top = 16.dp)
-            ) {
-                SettingsItem(
-                    titleResId = R.string.fragment_settings_vibrate,
-                    hintResId = R.string.fragment_settings_vibrate_hint,
-                    isChecked = true,
-                    clickListener = null
-                )
-                Divider(
-                    modifier = Modifier
-                        .padding(vertical = 8.dp)
-                        .background(ColorPrimary)
-                        .height(1.dp)
-                )
-                SettingsItem(
-                    titleResId = R.string.fragment_settings_flash,
-                    hintResId = R.string.fragment_settings_flash_hint,
-                    isChecked = true,
-                    clickListener = null
-                )
-                Divider(
-                    modifier = Modifier
-                        .padding(vertical = 8.dp)
-                        .background(ColorPrimary)
-                        .height(1.dp)
-                )
-                SettingsItem(
-                    titleResId = R.string.fragment_settings_save_scanned_barcodes_to_history,
-                    hintResId = R.string.fragment_settings_save_scanned_barcodes_to_history_hint,
-                    isChecked = false,
-                    clickListener = null
-                )
-                Divider(
-                    modifier = Modifier
-                        .padding(vertical = 8.dp)
-                        .background(ColorPrimary)
-                        .height(1.dp)
-                )
-                SettingsItem(
-                    titleResId = R.string.fragment_settings_sending_note,
-                    hintResId = R.string.fragment_settings_sending_note_hint,
-                    isChecked = true,
-                    clickListener = null
-                )
-                Divider(
-                    modifier = Modifier
-                        .padding(vertical = 8.dp)
-                        .background(ColorPrimary)
-                        .height(1.dp)
-                )
-            }
-        }
-    }
+    SettingsItem(title = "Вибрация", hint = "Вибрация при сканировании", isChecked = true, clickListener = null)
 }
 
 @Composable
 fun SettingsItem(
-    titleResId: Int,
-    hintResId: Int,
+    title: String,
+    hint: String,
     isChecked: Boolean,
     clickListener: ((Boolean) -> Unit)?
 ) {
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
+    Row {
         Column {
-            Text(text = stringResource(id = titleResId))
-            Spacer(modifier = Modifier.height(4.dp))
-            SecondaryText(text = stringResource(id = hintResId))
+            Text(text = title)
+            SecondaryText(text = hint)
         }
-        Switch(checked = isChecked, onCheckedChange = clickListener)
+        Checkbox(checked = isChecked, onCheckedChange = clickListener)
     }
 }
