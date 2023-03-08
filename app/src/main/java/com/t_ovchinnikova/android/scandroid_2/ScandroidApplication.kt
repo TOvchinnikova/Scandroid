60ef7a25552e0b8d99b3f7126a9aee4ab0889567
t.ovchinnikova <t.ovchinnikova@evotor.ru>
2023-03-08 10:30:33 +0500

    added list of coin modules

--- app/src/main/java/com/t_ovchinnikova/android/scandroid_2/ScandroidApplication.kt
+++ app/src/main/java/com/t_ovchinnikova/android/scandroid_2/ScandroidApplication.kt
@@ -1,10 +1,7 @@
 package com.t_ovchinnikova.android.scandroid_2
 
 import android.app.Application
-import com.t_ovchinnikova.android.scandroid_2.core_db_impl.di.databaseModule
-import com.t_ovchinnikova.android.scandroid_2.di.appModule
-import com.t_ovchinnikova.android.scandroid_2.di.dataModule
-import com.t_ovchinnikova.android.scandroid_2.di.domainModule
+import com.t_ovchinnikova.android.scandroid_2.di.appModules
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -17,7 +14,7 @@ class ScandroidApplication : Application() {
         startKoin {
             androidLogger(if (BuildConfig.DEBUG) Level.ERROR else Level.NONE)
             androidContext(this@ScandroidApplication)
-            modules(appModule, domainModule, dataModule, databaseModule)
+            modules(appModules)
         }
     }
 }
