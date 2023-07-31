073d1421b8af109a8bc92ab55ba30a48a2b2c6af
t.ovchinnikova <t.ovchinnikova@evotor.ru>
2023-07-31 22:46:13 +0500

    fixed note dialog

--- core-ui/src/main/java/com/t_ovchinnikova/android/scandroid_2/core_ui/DIalogs.kt
+++ core-ui/src/main/java/com/t_ovchinnikova/android/scandroid_2/core_ui/DIalogs.kt
@@ -4,12 +4,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.MaterialTheme.typography
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun SimpleAlertDialog(
@@ -63,25 +67,35 @@ fun AlertDialogWithTextField(
     val textState = rememberSaveable { mutableStateOf(text) }
 
     AlertDialog(
+        modifier = Modifier.wrapContentHeight(),
         title = { Text(text = title, style = typography.h6) },
         text = {
             subtitle?.let {
                 Text(it)
             }
-            Column(
-                verticalArrangement = Arrangement.Center
-            ) {
-                TextField(
-                    value = textState.value,
-                    onValueChange = { textState.value = it },
-                    label = {
-                        Text(text = textHint)
-                    },
-                    modifier = Modifier
-                        .background(Color.Transparent)
-                        .padding(top = 16.dp)
-                )
-            }
+            TextField(
+                textStyle = TextStyle(fontSize = 16.sp),
+                value = textState.value,
+                onValueChange = { textState.value = it },
+                label = {
+                    Text(text = textHint)
+                },
+                modifier = Modifier
+                    .background(Color.Transparent),
+                trailingIcon = {
+                    if (textState.value != EMPTY) IconButton(
+                        onClick = {
+                            textState.value = EMPTY
+                        },
+                        modifier = Modifier.size(20.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.Clear,
+                            contentDescription = "Clear"
+                        )
+                    }
+                }
+            )
         },
         confirmButton = {
             OutlinedButton(
@@ -91,16 +105,16 @@ fun AlertDialogWithTextField(
             ) {
                 Text(text = dismissButtonText)
             }
-                Button(
-                    onClick = {
-                        confirmClickListener(textState.value)
-                        dismissClickListener()
-                    },
-                    modifier = Modifier
-                        .padding(8.dp)
-                ) {
-                    Text(text = confirmButtonText)
-                }
+            Button(
+                onClick = {
+                    confirmClickListener(textState.value)
+                    dismissClickListener()
+                },
+                modifier = Modifier
+                    .padding(8.dp)
+            ) {
+                Text(text = confirmButtonText)
+            }
         },
         onDismissRequest = dismissClickListener
     )
