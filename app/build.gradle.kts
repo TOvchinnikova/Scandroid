145c95bc4b72bf9d5b1f023d8b76a91f9a11fb2c
t.ovchinnikova <t.ovchinnikova@evotor.ru>
2024-01-13 13:59:25 +0500

    versionCode is updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.t_ovchinnikova.android.scandroid_2"
         minSdk = 23
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
